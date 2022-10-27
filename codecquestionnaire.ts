ce1c9cbfc6af0ea2f19468192636cb01d278bab8
navin-22 <31242843+navin-22@users.noreply.github.com>
2022-10-27 03:10:08 +0100

    Update codecquestionnaire.ts
    Added comments

--- codecquestionnaire.ts
+++ codecquestionnaire.ts
@@ -1,3 +1,4 @@
+// Please the below code and test cases for the questionnaire
 const rightCommand = "R";
 const leftCommand = "L";
 const frontCommand = "F";
@@ -12,6 +13,7 @@ const testInputCommands = ["FFRFLFLF", "RRRF", "FRFFFLLFRRFF", "RRRFLFR"];
 const testOutputPosition = [[1,4,"W"], [1,1,"W"], [3,2,"E"], [1,1,"W"]];
 runTestCases()
 
+// This method is used for running all test cases at once.
 function runTestCases() {
     var i = 0;
     while (i < testInputCommands.length) {
@@ -19,8 +21,9 @@ function runTestCases() {
     }
 }
 
-function caseCheck(input: any, inputCommand: string, requiredOutput: any, testCase: number) {
-    var obtainedOutput = findPosition(input[0], input[1], inputCommand);
+// this method checks each test case with the obtained output to whether it passed or failed
+function caseCheck(inputPlateauSize: any, inputCommand: string, requiredOutput: any, testCase: number) {
+    var obtainedOutput = findPosition(inputPlateauSize[0], inputPlateauSize[1], inputCommand);
     if (obtainedOutput.every((element, index) => element == requiredOutput[index])) {
         console.log("TestCase" + testCase + " Passed");
     } else {
@@ -28,18 +31,23 @@ function caseCheck(input: any, inputCommand: string, requiredOutput: any, testCa
     }
 }
 
-function findPosition(inputX: number, inputY: number, command: string) {
+// This method is used find the position of the robot after an command is given
+// Here we have 3 commands, R and L which are directions and F for movement.
+// we also have 4 constriants that limit the movement of robot.
+function findPosition(plateauSizeX: number, plateauSizeY: number, command: string) {
     var direction = Direction.North;
     const input = command.toUpperCase();
     var currentPosX = 1;
     var currentPosY = 1;
     var i = 0;
     for (var i = 0; i < input.length; i++) {
-        if (input[i] != frontCommand) {
+        if (input[i] != frontCommand) { // to the direction if the given command is direction command
             direction = setDirection(input[i], direction)
         } else {
-            if (!checkConstraints(currentPosX, currentPosY, direction, inputX, inputY)) continue;
+            // if its a movement command, checking if there is further movements available on the plateau
+            if (!checkConstraints(currentPosX, currentPosY, direction, plateauSizeX, plateauSizeY)) continue;
             else {
+                // if movements are available on plateau, then new position of robot is recorded.
                 const position = setNewPosition(currentPosX, currentPosY, direction)
                 currentPosX = position[0];
                 currentPosY = position[1];
@@ -49,6 +57,7 @@ function findPosition(inputX: number, inputY: number, command: string) {
     return [currentPosX, currentPosY, direction]
 }
 
+// this method sets the new position of the robot.
 function setNewPosition(posX: number, posY: number, currentDirection: string) {
     if (currentDirection == Direction.North) {
         posY++;
@@ -62,6 +71,7 @@ function setNewPosition(posX: number, posY: number, currentDirection: string) {
     return [posX, posY];
 }
 
+// this method sets the new direction for the robot when a direction command is encountered 
 function setDirection(currentInput: string, currentDirection: string) {
    const direction = [Direction.North, Direction.East, Direction.South, Direction.West]; 
    if (currentInput == rightCommand) {
@@ -77,11 +87,13 @@ function setDirection(currentInput: string, currentDirection: string) {
    }
 }
 
+// this method checks the constraints for the movement of robot when a movement command is encountered
+// there are 4 constraints that can prevent the movement of robot.
 function checkConstraints(posX: number, posY: number, currentDirection: string, 
-                            inputX: number, inputY: number) {
+                            inputPlateauX: number, inputPlateauY: number) {
      if ((posX == 1 && currentDirection == Direction.South) ||
-     (posX == inputX && currentDirection == Direction.East) ||
+     (posX == inputPlateauX && currentDirection == Direction.East) ||
      (posY == 1 && currentDirection == Direction.West) || 
-     (posY == inputY && currentDirection == Direction.North)) return 0;
+     (posY == inputPlateauY && currentDirection == Direction.North)) return 0;
     return 1;
 }
